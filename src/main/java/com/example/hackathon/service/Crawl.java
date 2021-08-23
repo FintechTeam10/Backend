bd48629a89aafe3bd171b3dbac82de98ba537530
CodeDiary18 <codediary18@gmail.com>
2021-08-23 16:11:37 +0900

    Windows 버전

--- src/main/java/com/example/hackathon/service/Crawl.java
+++ src/main/java/com/example/hackathon/service/Crawl.java
@@ -67,7 +67,7 @@ public class Crawl {
     @Async
     public void crawl(Long loan_id, String find, String channel, boolean secondTime, Long crawl_id) throws IOException {
 
-        String filePath = "/root/chromedriver";
+        String filePath = "C:\\Tools\\chromedriver.exe";
         CrawledDto crawledDto = new CrawledDto();
         crawledDto.setLoanId(loan_id);
       
